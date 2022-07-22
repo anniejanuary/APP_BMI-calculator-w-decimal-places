95b49b268da39efd569347dec686a649684e716d
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-22 12:21:56 +0200

    Update BMI float 1 decimal place.py

--- BMI float 1 decimal place.py
+++ BMI float 1 decimal place.py
@@ -1,16 +1,51 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 
-height_float=float(height)
-weight_float=float(weight)
+#     1 __ BMI as a rounded int :
 
-BMI=weight_float/height_float**2
+BMI=round(weight/height**2)
 
-#     formatting a float to 2 decimal places :
-BMI_format="{:.1f}".format(BMI)
-print(f"Your BMI is: {BMI_format}")
+if BMI < 18.5:
+  print(f"Your BMI is: {BMI}, you are are underweight")
+elif BMI < 25:
+  print(f"Your BMI is: {BMI}, you have a normal weight")
+elif BMI < 30:
+  print(f"Your BMI is: {BMI}, you are slightly overweight")
+elif BMI < 35:
+  print(f"Your BMI is: {BMI}, you are obese")
+else:
+  print(f"Your BMI is: {BMI}, you are clinically obese")
+  
+  
+#     2 A __ BMI as a float to X decimal places : 
 
-#     or formatting a float to int for an int BMI
-BMI_format=int(BMI)
-print(f"Your BMI is: {BMI_format}")
+BMI=weight/height**2
+BMI_format="{:.2f}".format(BMI)
 
+if float(BMI_format) < 18.5:
+  print(f"Your BMI is: {BMI_format}, you are are underweight")
+elif float(BMI_format) >= 18.5:
+  print(f"Your BMI is: {BMI_format}, you have a normal weight")
+elif float(BMI_format) >= 25:
+  print(f"Your BMI is: {BMI_format}, you are slightly overweight")
+elif float(BMI_format) >= 30:
+  print(f"Your BMI is: {BMI_format}, you are obese")
+else:
+  print(f"Your BMI is: {BMI_format}, you are clinically obese")
+
+
+  #     2 B __ BMI as a float to X decimal places not showing 0 if it's in the last place :
+  
+BMI=weight/height**2
+BMI_format=round(BMI, 2)
+
+if float(BMI_format) < 18.5:
+  print(f"Your BMI is: {BMI_format}, you are are underweight")
+elif float(BMI_format) >= 18.5:
+  print(f"Your BMI is: {BMI_format}, you have a normal weight")
+elif float(BMI_format) >= 25:
+  print(f"Your BMI is: {BMI_format}, you are slightly overweight")
+elif float(BMI_format) >= 30:
+  print(f"Your BMI is: {BMI_format}, you are obese")
+else:
+  print(f"Your BMI is: {BMI_format}, you are clinically obese")
